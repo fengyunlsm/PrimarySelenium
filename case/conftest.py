cd3f77d6ecf0873e21c05e9d2aa3f0463a4348fd
fengyunlsm <2399548030@qq.com>
2020-11-21 17:38:27 +0800

    aaa

--- case/conftest.py
+++ case/conftest.py
@@ -8,6 +8,6 @@ from base.basepage import SeleniumDriver
 def init_driver():
     driver = SeleniumDriver("chrome")
     driver.get_url('http://eshop.tslj.cn/#/login')
-    driver.handle_window('max')
+    # driver.handle_window('max')
     yield driver
     driver.close_driver()
